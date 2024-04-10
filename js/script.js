7e05859bce3179bbc263a37c47a6f3d151f56c9f
Zosia Samosia <Zosia@Samosia.pl>
2024-04-10 17:02:51 +0200

    Navigation

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict';
+const menuBtn = document.querySelector('span');
+const menuList = document.querySelector('#navbox');
+
+function OpenMenu()
+{
+    if(menuBtn.innerHTML == 'menu')
+    {
+        menuBtn.innerHTML = 'close';
+        menuList.classList.add('active');
+    }
+    else{
+        menuBtn.innerHTML = 'menu';
+        menuList.classList.remove('active');
+    }
+}
